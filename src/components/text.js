64761d519afbab6ca8533c07eb0340f2ac6157e8
priyankasharmak <priyankasharmak21@gmail.com>
2024-02-29 15:35:30 +0530

    image

--- src/components/text.js
+++ src/components/text.js
@@ -4,12 +4,12 @@ import styles from './text.module.css'
 
 const Tex = () => {
     return ( 
-        <div >
+        <div className={styles.bg} >
             <div className={styles.box}>Every pitch deck <br /> deserves a story</div>
             <div className={styles.box1}>Tell your story with a Journey -- responsive slides, video recoding,interactive blocks and embeds from your favorite</div>
             <div className={styles.bt}>
                 <button className={styles.box2}>Create an account</button>
-                <button className={styles.box3}>With google</button>
+                <button className={styles.box3}><img src="https://cdn.iconscout.com/icon/free/png-512/free-google-160-189824.png?f=webp&w=20" alt="" />With google</button>
                 <button className={styles.box4}>With microsoft</button>
                 <button className={styles.box5}>Try it now</button>
             </div>
